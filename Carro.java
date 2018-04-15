f8c5741d5798d0fc23d400709fa38e96736f2e90
Caio Sousa <sousinhacaio@gmail.com>
2018-04-14 21:50:47 -0300

    Create Carro.java

---
+++ Carro.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Carro{
+
+   //Método para gerar um Carro em um lugar aleatório da matriz
+  	public void gerarCarro(){
+  	   c.add(new Carro());
+			  Random random= new Random();
+			  int a =random.nextInt(29);
+        int b =random.nextInt(59);
+			  if(matrix[a][b] == 0 && matrix[a][b]!=2 && matrix[a][b]!=1 && matrix[a][b]!=4 && matrix[a][b]!=5){
+			    matrix[a][b]=3;
+			  
+		}
+		
+		}
+}
