852b69886b9eac91ddec85a4fc4a2bed5094c880
Caio Sousa <sousinhacaio@gmail.com>
2018-04-14 21:51:29 -0300

    Create Moto.java

---
+++ Moto.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+public class Moto{
+  
+   //Método para gerar uma Moto em um lugar aleatório da matriz
+  public void gerarMoto(){
+    m.add(new Moto());
+    Random random= new Random();
+			  int a =random.nextInt(29);
+        int b =random.nextInt(59);
+			  if(matrix[a][b] == 0 && matrix[a][b]!=2 && matrix[a][b]!=1 && matrix[a][b]!=3 && matrix[a][b]!=4){
+			    matrix[a][b]=4;
+			  
+		}
+	
+  }
+}
