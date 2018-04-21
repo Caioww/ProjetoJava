54d5e50fdba48f1f3578fc6910900d9ebd8e0856
Caio Sousa <sousinhacaio@gmail.com>
2018-04-21 20:46:34 -0300

    Update Moto.java
    Moto agora esta se movimentando

--- Moto.java
+++ Moto.java
@@ -1,16 +1,42 @@
 import java.util.Random;
-public class Moto{
-  
-   //Método para gerar uma Moto em um lugar aleatório da matriz
-  public void gerarMoto(){
-    m.add(new Moto());
-    Random random= new Random();
-			  int a =random.nextInt(29);
-        int b =random.nextInt(59);
-			  if(matrix[a][b] == 0 && matrix[a][b]!=2 && matrix[a][b]!=1 && matrix[a][b]!=3 && matrix[a][b]!=4){
-			    matrix[a][b]=4;
-			  
+
+public class Moto extends Veiculo{
+	Veiculo v = new Veiculo();
+	
+	public Moto (int x, int y, int velocidade, String cor) {
+		super(x, y, velocidade, cor);
+	}
+	
+	public void move(Moto m) {
+		mov = r.nextInt(4);
+		
+		if(mov == 0) {
+			int x = m.getX();
+			x = x + 3;
+			m.andaX(x);
+		}
+		
+		if(mov == 1) {
+			int x = m.getX();
+			x = x - 3;
+			m.andaX(x);
+		}
+		
+		if(mov == 2) {
+			int y = m.getY();
+			y = y + 3;
+			m.andaY(y);
+		}
+		
+		if(mov == 3) {
+			int y = m.getY();
+			y = y - 3;
+			m.andaY(y);
 		}
 	
-  }
+	}
+	
+	private int mov;
+	private String tipo;
+
 }
