568e5c08991af48552829405cae37c828fa3b8c2
Caio Sousa <sousinhacaio@gmail.com>
2018-04-21 20:46:00 -0300

    Update Carro.java
    Melhorando classe carro , agora com movimento utilizando construtor

--- Carro.java
+++ Carro.java
@@ -1,16 +1,47 @@
 import java.util.Random;
-public class Carro{
 
-   //Método para gerar um Carro em um lugar aleatório da matriz
-  	public void gerarCarro(){
-  	   c.add(new Carro());
-			  Random random= new Random();
-			  int a =random.nextInt(29);
-        int b =random.nextInt(59);
-			  if(matrix[a][b] == 0 && matrix[a][b]!=2 && matrix[a][b]!=1 && matrix[a][b]!=4 && matrix[a][b]!=5){
-			    matrix[a][b]=3;
-			  
+public class Carro extends Veiculo{
+	Veiculo v = new Veiculo();
+	
+	public Carro (int x, int y, int velocidade, String cor) {
+		super(x, y, velocidade, cor);
+	}
+	
+	Random r = new Random();
+	
+	public void move(Carro c) {
+		mov = r.nextInt(4);
+		
+		if(mov == 0) {
+			int x = c.getX();
+			x = x + 2;
+			c.andaX(x);
 		}
 		
+		if(mov == 1) {
+			int x = c.getX();
+			x = x - 2;
+			c.andaX(x);
 		}
+		
+		if(mov == 2) {
+			int y = c.getY();
+			y = y + 2;
+			c.andaY(y);
+		}
+		
+		if(mov == 3) {
+			int y = c.getY();
+			y = y - 2;
+			c.andaY(y);
+		}
+		
+	}
+
+	public int getMov() {
+		return mov;
+	}
+	
+	private int num_passageiros;
+	private int mov;
 }
