b2d1273984a2c96779ce78d9b10e216d3076f12e
Caio Sousa <sousinhacaio@gmail.com>
2018-04-22 21:22:44 -0300

    Só uns comentários
    Comentários para doxygen

--- Main.java
+++ Main.java
@@ -1,21 +1,19 @@
+/**
+ * @author Caio
+ */
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
 		Mundo m = new Mundo();
-
-		m.geraVeiculos();
-
-		m.desenhaMundo();
+		
+		m.geraVeiculos(); 
+		m.desenhaMundo(); 
 		
 		int i = 0;
-		while (i == 0) {
-			m.atualizaMundo();
-			Thread.sleep(500);  
-		}
 		
-	
-
-
+		while (i == 0) {
+			m.atualizaMundo(); 
+			Thread.sleep(300);  
+		}
 	}
-
 }
