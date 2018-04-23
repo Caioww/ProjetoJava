4fb48fae2cda1191a3437a31dbf0778ba81aadde
Caio Sousa <sousinhacaio@gmail.com>
2018-04-22 21:02:50 -0300

    Update Carro.java
    Pequenas mudanças ,redução de umas variaveis inutilizadas e comentários para fazer a documentação no Doxygen.

--- Carro.java
+++ Carro.java
@@ -1,47 +1,104 @@
+/**
+ * 
+ * Classe que gera um veículo do tipo Carro
+ * 
+ * @author Caio
+ * 
+ * @see Veiculo
+ */
 import java.util.Random;
 
 public class Carro extends Veiculo{
 	Veiculo v = new Veiculo();
 	
-	public Carro (int x, int y, int velocidade, String cor) {
-		super(x, y, velocidade, cor);
+	/**
+	 * Construtor da classe Carro, que usa um Super para chamar o construtor da classe veiculo
+	 * 
+	 * @see Veiculo
+	 * @param x
+	 * @param y
+	 * @param velocidade
+	 * @param cor
+	 * @param fabrica
+	 */
+	public Carro (int x, int y, int velocidade, String cor, boolean fabrica) {
+		super(x, y, velocidade, cor, fabrica);
 	}
 	
-	Random r = new Random();
+	/**
+	 * Random utilizado para gerar movimentos aleatórios na movimentação do Carro
+	 */
+	Random aleatorio = new Random();
 	
-	public void move(Carro c) {
-		mov = r.nextInt(4);
+	/**
+	 * Função que movimenta o carro, recebendo um objeto da própria classe como parâmetro para armazenar as posições dos eixos individualmente
+	 * @param c
+	 */
+	public void move(Carro car) {
+		mover = aleatorio.nextInt(4); ///<Gerar posição aleatória , sendo cada numero é equivalente a uma direção de 0 á 3.
 		
-		if(mov == 0) {
-			int x = c.getX();
-			x = x + 2;
-			c.andaX(x);
+		/**
+		 *Verificação do random para movimentar o caminhão ,com a utilização de getters 
+		 */
+		if(mover == 0) {
+			int x = car.getX(); /
+			x = x + 2; 
+			car.andaX(verificaX(x)); 
 		}
 		
-		if(mov == 1) {
-			int x = c.getX();
+	
+		 
+		if(mover == 1) {
+			int x = car.getX();
 			x = x - 2;
-			c.andaX(x);
+			car.andaX(verificaX(x));
 		}
 		
-		if(mov == 2) {
-			int y = c.getY();
+		if(mover == 2) {
+			int y = car.getY();
 			y = y + 2;
-			c.andaY(y);
+			car.andaY(verificaY(y));
 		}
 		
-		if(mov == 3) {
-			int y = c.getY();
+		if(mover == 3) {
+			int y = car.getY();
 			y = y - 2;
-			c.andaY(y);
+			car.andaY(verificaY(y));
 		}
 		
+	}
+	
+	/**
+	 * Função para verificar se o carro chegou ao limite da matriz(mapa) em X e reseta a coordenada para a posição inicial
+	 * @param x
+	 * @return
+	 */
+	public int verificaX(int x) {
+		if (x >= 29) {
+			x = 1;
+		}
+		if(x <= 0) {
+			x = 28;
+		}
+		return x;
+	}
+	
+	/**
+	 * Função para verificar se o carro chegou ao limite da matriz(mapa) em Y e reseta a coordenada para a posição inicial
+	 * @param y
+	 * @return
+	 */
+	public int verificaY(int y) {
+		if (y >= 59) {
+			y = 1;
+		}
+		if(y <= 0) {
+			y = 58;
+		}
+		return y;
 	}
 
-	public int getMov() {
-		return mov;
-	}
 	
-	private int num_passageiros;
-	private int mov;
+
+	private int mover; /// Variável de movimentação do Carro
 }
