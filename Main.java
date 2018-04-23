e368da3fe3c1bef99ca68e26d9e91f682734268f
Caio Sousa <sousinhacaio@gmail.com>
2018-04-22 23:15:56 -0300

    Fim da Classe Main
    Fim dos comentários e ajustes no while

--- Main.java
+++ Main.java
@@ -4,16 +4,23 @@
 public class Main {
 
 	public static void main(String[] args) throws InterruptedException {
+	  /**
+	   *Instanciando classe mundo para utilizar seus atributos e funções no Main 
+	   */
 		Mundo m = new Mundo();
-		
+		/**
+		 * Gerar mundo com veiculos iniciais
+		 * */
 		m.geraVeiculos(); 
-		m.desenhaMundo(); 
+		m.desenhandoMundo(); 
 		
 		int i = 0;
-		
+		/**
+		 *Laço de repetição para o mapa ser atualizado e os veiculos se moverem respeitando as condições da classe Mundo 
+		 */ 
 		while (i == 0) {
-			m.atualizaMundo(); 
-			Thread.sleep(300);  
+			m.mundoAtualizado(); 
+			Thread.sleep(400);  
 		}
 	}
 }
