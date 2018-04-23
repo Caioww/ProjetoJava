b0369750783bb6ad5b91a1e9f5857f031c3f5b84
Caio Sousa <sousinhacaio@gmail.com>
2018-04-22 21:20:49 -0300

    Pequenas alterações

--- Mundo.java
+++ Mundo.java
@@ -1,7 +1,13 @@
 import java.util.ArrayList;
 
 public class Mundo {
+	/**
+	 * Instanciando a classe veículo
+	 */
 	Veiculo v = new Veiculo();
+	/**
+	 * Gerando ArrayList de cada um dos veículos
+	 */
 	ArrayList <Carro> ca = new ArrayList<>();
 	ArrayList <Caminhao> da = new ArrayList<>();
 	ArrayList <Moto> ma = new ArrayList<>();
@@ -37,10 +43,13 @@ public class Mundo {
             				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
             				{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}};
 	
+	/**
+	 * Função que gera veículos aleatóriamente
+	 */
 	public void geraVeiculos() {
 		for(int i = 0; i < 10; i++) {
 			ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
-			while (mapa[ca.get(i).getX()][ca.get(i).getY()] == 2) {
+			while (mapa[ca.get(i).getX()][ca.get(i).getY()] == 2) { /**< Verificando se o carro foi gerado onde há uma fábrica, se sim, trocando-o */
 				ca.get(i).setX();
 				ca.get(i).setY();
 			}
@@ -59,6 +68,9 @@ public class Mundo {
 		}		
 	}
 	
+	/**
+	 * Função que zera o mapa, acabando com o rastro que ficaria caso o mapa não fosse zerado
+	 */
 	public void refazMapa() {
 		for(int i = 0; i<30; i++) {
 			for(int j = 0; j < 60; j++) {
@@ -67,9 +79,61 @@ public class Mundo {
 				}
 			}
 		}
+		/**
+		 * Esse é um comentário a parte, mas que achei legal estar na documentação:
+		 * Parte do código que alteraria a situação de um veículo que não está mais em uma fábrica para false
+		 * Essa parte foi comentada porém não removida pois faz a quantidade de veículos aumentar ao invés de zerar, como no vídeo de exemplo
+		 * Apesar de ser o mais lógico colocar um veículo fora de uma fábrica como false, decidi remover do código final
+		 * Removi pois o funcionamento do vídeo de exemplo se asemelha mais com o código funcionando sem essa parte
+		 * 
+		for(int a = 0; a < ca.size(); a++) {
+			int xa = ca.get(a).getX(); 
+			int ya = ca.get(a).getY();
+			for(int i = 0; i<30; i++) {
+				for(int j = 0; j < 60; j++) {
+					if(i == xa && j == ya) {
+						if(mapa[i][j] != 2 && ca.get(a).getFabrica() == true) {
+							ca.get(a).setFabrica(false);
+						}
+					}
+				}
+			}
+		}
 		
+		for(int a = 0; a < da.size(); a++) {
+			int xa = da.get(a).getX(); 
+			int ya = da.get(a).getY();
+			for(int i = 0; i<30; i++) {
+				for(int j = 0; j < 60; j++) {
+					if(i == xa && j == ya) {
+						if(mapa[i][j] != 2 && da.get(a).getFabrica() == true) {
+							da.get(a).setFabrica(false);
+						}
+					}
+				}
+			}
+		}
+		
+		for(int a = 0; a < da.size(); a++) {
+			int xa = da.get(a).getX(); 
+			int ya = da.get(a).getY();
+			for(int i = 0; i<30; i++) {
+				for(int j = 0; j < 60; j++) {
+					if(i == xa && j == ya) {
+						if(mapa[i][j] != 2 && da.get(a).getFabrica() == true) {
+							da.get(a).setFabrica(false);
+						}
+					}
+				}
+			}
+		}
+		*/
+		/**
+		 * Adicionando os novos veículos no mapa
+		 */
 		for (int a = 0; a < ca.size(); a++) {
-			int xa = ca.get(a).getX();
+			/// Obtendo as corrdenadas do veículo
+			int xa = ca.get(a).getX(); 
 			int ya = ca.get(a).getY();
 			
 
@@ -77,7 +141,7 @@ public class Mundo {
 				for(int j = 0; j < 60; j++) {
 					if(i == xa && j == ya) {
 						if(mapa[i][j] == 2) {
-							mapa[i][j] = 2;
+							mapa[i][j] = 2; /// SObrescrevendo 2 caso seja uma fábrica
 							
 						}
 						else {
@@ -128,32 +192,24 @@ public class Mundo {
 		
 	}
 	
+	/**
+	 * Função que desenha o mundo
+	 */
 	public void desenhaMundo() {
 		
+		/**
+		 * Contadores para exibir quantidade de veículos
+		 */
 		int carros = 0;
 		int motos = 0;
 		int caminhoes = 0;
 		
-		for(int i = 0; i < ca.size(); i++) {
-			if(ca.get(i).getFabrica() == true) {
-				carros++;
-			}
-		}
-		
-		for(int i = 0; i < da.size(); i++) {
-			if(da.get(i).getFabrica() == true) {
-				caminhoes++;
-			}
-		}
-		
-		for(int i = 0; i < ma.size(); i++) {
-			if(ma.get(i).getFabrica() == true) {
-				motos++;
-			}
-		}
 			for(int i = 0; i<30; i++) {
 				for(int j = 0; j < 60; j++) {
 					
+					/**
+					 * Desenhando o mapa com base no que foi refeito
+					 */
 					if(mapa[i][j] == 1) {
 						System.out.print("\u001b[47m \033[0m");
 					}
@@ -162,23 +218,16 @@ public class Mundo {
 					}
 					
 					else if(mapa[i][j] == 2) {
-						//System.out.print("X");
 						System.out.print("\u001b[42m \033[0m");
 					}
 					else if(mapa[i][j] == 3){
-						//System.out.print("C");
 						System.out.print("\u001b[44m \033[0m");
-						carros++;
 					}
 					else if(mapa[i][j] == 4){
-						//System.out.print("D");
 						System.out.print("\u001b[43;1m \033[0m");
-						caminhoes++;
 					}
 					else if(mapa[i][j] == 5){
-						//System.out.print("M");
 						System.out.print("\u001b[41m \033[0m");
-						motos++;
 					}
 					
 				}
@@ -186,17 +235,38 @@ public class Mundo {
 				System.out.println("");
 			}	
 			
-			/*System.out.print("\u001b[44m \033[0m");
+			/**
+			 * Imprimindo a legenda
+			 */
+			System.out.print("\u001b[44m \033[0m");
 			System.out.print(" Carros   ");
 			System.out.print("\u001b[41m \033[0m");
 			System.out.print(" Motos   ");
 			System.out.print("\u001b[43;1m \033[0m");
-			System.out.println(" Caminhões");*/
+			System.out.println(" Caminhões");
+			
+			/**
+			 * Contadores de veículos
+			 */
+			for(int a = 0; a < ca.size(); a++) {
+				carros++;
+			}
+			for(int a = 0; a < da.size(); a++) {
+				caminhoes++;
+			}
+			for(int a = 0; a < ma.size(); a++) {
+				motos++;
+			}
+			
+		
 			System.out.println("Número de carros: " + carros);
 			System.out.println("Número de caminhões: " + caminhoes);
 			System.out.println("Número de motos: " + motos);
 	}
 	
+	/**
+	 * Função que atualiza o mundo fazendo os veículos se moverem
+	 */
 	public void atualizaMundo() {
 		for(int i = 0; i < ca.size(); i++) {
 			ca.get(i).move(ca.get(i));
@@ -216,62 +286,53 @@ public class Mundo {
 		desenhaMundo();
 	}
 	
+	/**
+	 * Função que detecta colisão
+	 */
 	public void detectaColisao() {
-		// Colisão entre carros apenas
+		/// Colisão entre carros apenas
 		for (int i = 0; i < ca.size(); i ++) {
 			for (int j = 0; j < ca.size(); j++) {
-				if(ca.get(j).getX() == ca.get(i).getX() && ca.get(j).getY() == ca.get(i).getX()) {
-					if(ca.get(i).getX() == ca.get(i).getX() && ca.get(i).getY() == ca.get(i).getY()) {
+				if(ca.get(j).getX() == ca.get(i).getX() && ca.get(j).getY() == ca.get(i).getY()) {
+					if (i == j) { /// Essa parte verifica se o veículo é ele mesmo, e não o remove
 						
 					}
 					else {
 						ca.remove(ca.get(i));
-						ca.remove(ca.get(j));
-					}
-						
-					
-					
+					}	
 				}
 			}
 		}
 		
-		// Colisão entre caminhões apenas
+		/// Colisão entre caminhões apenas
 		for (int i = 0; i < da.size(); i ++) {
 			for (int j = 0; j < da.size(); j++) {
 				if(da.get(j).getX() == da.get(i).getX() && da.get(j).getY() == da.get(i).getY()) {
-					if(da.get(i).getX() == da.get(i).getX() && da.get(i).getY() == da.get(i).getY()) {
+					if(i == j) {
 						
 					}
 					else {
 						da.remove(da.get(i));
-						da.remove(da.get(j));
-					}
-					
-					
-						
+					}	
 				}
 			}
 		}
 		
-		// Colisão entre motos apenas
+		/// Colisão entre motos apenas
 		for (int i = 0; i < ma.size(); i ++) {
 			for (int j = 0; j < ma.size(); j++) {
 				if(ma.get(j).getX() == ma.get(i).getX() && ma.get(j).getY() == ma.get(i).getY()) {
-					if(ma.get(i).getX() == ma.get(i).getX() && ma.get(i).getY() == ma.get(i).getY()) {
+					if(i == j) {
 						
 					}
 					else {
 						ma.remove(ma.get(i));
-						ma.remove(ma.get(j));
-					}
-						
-					
-					
+					}	
 				}
 			}
 		}
 		
-		// Colisão entre caminhão e carro
+		/// Colisão entre caminhão e carro
 		for(int i = 0; i < da.size(); i++) {
 			for (int j = 0; j < ca.size(); j++) {
 				if(ca.get(j).getX() == da.get(i).getX() && ca.get(j).getY() == da.get(i).getY()) {
@@ -281,14 +342,11 @@ public class Mundo {
 					else {
 						ca.remove(ca.get(j));
 					}
-						
-					
-					
 				}
 			}
 		}
 		
-		// Colisão entre caminhão e moto
+		/// Colisão entre caminhão e moto
 		for(int i = 0; i < da.size(); i++) {
 			for (int j = 0; j < ma.size(); j++) {
 				if(ma.get(j).getX() == da.get(i).getX() && ma.get(j).getY() == da.get(i).getY()) {
@@ -298,14 +356,11 @@ public class Mundo {
 					else {
 						ma.remove(ma.get(j));
 					}
-						
-					
-					
 				}
 			}
 		}
 		
-		// Colisão entre carro e moto
+		/// Colisão entre carro e moto
 		for(int i = 0; i < ca.size(); i++) {
 			for (int j = 0; j < ma.size(); j++) {
 				if(ma.get(j).getX() == ca.get(i).getX() && ma.get(j).getY() == ca.get(i).getY()) {
@@ -313,7 +368,7 @@ public class Mundo {
 						
 					}
 					else {
-						ma.remove(ma.get(j));
+						ma.remove(ma.get(i));
 					}
 					
 				}
@@ -321,6 +376,9 @@ public class Mundo {
 		}
 	}
 	
+	/**
+	 * Função que gera um veículo se passarem em fábricas
+	 */
 	public void geraVeiculo() {
 		for(int a = 0; a < ca.size(); a++) {
 			int x = ca.get(a).getX();
@@ -329,7 +387,7 @@ public class Mundo {
 					for(int j = 0; j < 60; j++) {
 						if(i == x && j == y) {
 							if(mapa[i][j] == 2 && ca.get(a).getFabrica() == false) {
-								ca.get(a).setFabrica(true);
+								ca.get(a).setFabrica(true); /// Já que o veículo passou numa fábrica, ele fica impossibilitado de gerar novamente caso não saia da fabrica
 								ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
 								}
 							}
