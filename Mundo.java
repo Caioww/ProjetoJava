783d8cae85a6f751a5dd48fef6af19170612bcc9
Caio Sousa <sousinhacaio@gmail.com>
2018-04-22 22:23:10 -0300

    Melhora em nome de funções , objetos e etc

--- Mundo.java
+++ Mundo.java
@@ -8,9 +8,9 @@ public class Mundo {
 	/**
 	 * Gerando ArrayList de cada um dos veículos
 	 */
-	ArrayList <Carro> ca = new ArrayList<>();
-	ArrayList <Caminhao> da = new ArrayList<>();
-	ArrayList <Moto> ma = new ArrayList<>();
+	ArrayList <Carro> car = new ArrayList<>();
+	ArrayList <Caminhao> cam = new ArrayList<>();
+	ArrayList <Moto> mot = new ArrayList<>();
 
 	public int mapa[][] = {{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
             				{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
@@ -48,22 +48,22 @@ public class Mundo {
 	 */
 	public void geraVeiculos() {
 		for(int i = 0; i < 10; i++) {
-			ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
-			while (mapa[ca.get(i).getX()][ca.get(i).getY()] == 2) { /**< Verificando se o carro foi gerado onde há uma fábrica, se sim, trocando-o */
-				ca.get(i).setX();
-				ca.get(i).setY();
+			car.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
+			while (mapa[car.get(i).getX()][car.get(i).getY()] == 2) { /**< Verificando se o carro foi gerado onde há uma fábrica, se sim, trocando-o */
+				car.get(i).setX();
+				car.get(i).setY();
 			}
 
-			da.add(new Caminhao((v.setX()+1), (v.setY()+1), 1, "amarelo", false));
-			while (mapa[da.get(i).getX()][da.get(i).getY()] == 2) {
-				da.get(i).setX();
-				da.get(i).setY();
+			cam.add(new Caminhao((v.setX()+1), (v.setY()+1), 1, "amarelo", false));
+			while (mapa[cam.get(i).getX()][cam.get(i).getY()] == 2) {
+				cam.get(i).setX();
+				cam.get(i).setY();
 			}
 
-			ma.add(new Moto((v.setX()+1), (v.setY()+1), 3, "vermelho", false));
-			while (mapa[ma.get(i).getX()][ma.get(i).getY()] == 2) {
-				ma.get(i).setX();
-				ma.get(i).setY();
+			mot.add(new Moto((v.setX()+1), (v.setY()+1), 3, "vermelho", false));
+			while (mapa[mot.get(i).getX()][mot.get(i).getY()] == 2) {
+				mot.get(i).setX();
+				mot.get(i).setY();
 			}
 		}		
 	}
@@ -71,7 +71,7 @@ public class Mundo {
 	/**
 	 * Função que zera o mapa, acabando com o rastro que ficaria caso o mapa não fosse zerado
 	 */
-	public void refazMapa() {
+	public void refazerMapa() {
 		for(int i = 0; i<30; i++) {
 			for(int j = 0; j < 60; j++) {
 				if(mapa[i][j] != 1 && mapa[i][j] != 2) {
@@ -83,10 +83,10 @@ public class Mundo {
 		/**
 		 * Adicionando os novos veículos no mapa
 		 */
-		for (int a = 0; a < ca.size(); a++) {
+		for (int a = 0; a < car.size(); a++) {
 			/// Obtendo as corrdenadas do veículo
-			int xa = ca.get(a).getX(); 
-			int ya = ca.get(a).getY();
+			int xa = car.get(a).getX(); 
+			int ya = car.get(a).getY();
 			
 
 			for(int i = 0; i<30; i++) {
@@ -104,9 +104,9 @@ public class Mundo {
 			}
 		}
 		
-		for(int a  = 0; a < da.size(); a++) {
-			int ba = da.get(a).getX();
-			int ca = da.get(a).getY();
+		for(int a  = 0; a < cam.size(); a++) {
+			int ba = cam.get(a).getX();
+			int ca = cam.get(a).getY();
 			
 			for(int i = 0; i<30; i++) {
 				for(int j = 0; j < 60; j++) {
@@ -123,9 +123,9 @@ public class Mundo {
 			}
 		}
 		
-		for(int a = 0; a < ma.size(); a ++) {
-			int ea = ma.get(a).getX();
-			int fa = ma.get(a).getY();
+		for(int a = 0; a < mot.size(); a ++) {
+			int ea = mot.get(a).getX();
+			int fa = mot.get(a).getY();
 			
 			for(int i = 0; i<30; i++) {
 				for(int j = 0; j < 60; j++) {
@@ -200,13 +200,13 @@ public class Mundo {
 			/**
 			 * Contadores de veículos
 			 */
-			for(int a = 0; a < ca.size(); a++) {
+			for(int a = 0; a < car.size(); a++) {
 				carros++;
 			}
-			for(int a = 0; a < da.size(); a++) {
+			for(int a = 0; a < cam.size(); a++) {
 				caminhoes++;
 			}
-			for(int a = 0; a < ma.size(); a++) {
+			for(int a = 0; a < mot.size(); a++) {
 				motos++;
 			}
 			
@@ -220,21 +220,21 @@ public class Mundo {
 	 * Função que atualiza o mundo fazendo os veículos se moverem
 	 */
 	public void atualizaMundo() {
-		for(int i = 0; i < ca.size(); i++) {
-			ca.get(i).movimento(ca.get(i));
+		for(int i = 0; i < car.size(); i++) {
+			car.get(i).movimento(car.get(i));
 		}
 		
-		for (int i = 0; i < da.size(); i ++) {
-			da.get(i).movimento(da.get(i));
+		for (int i = 0; i < cam.size(); i ++) {
+			cam.get(i).movimento(cam.get(i));
 		}
 		
-		for (int i = 0; i < ma.size(); i ++) {
-			ma.get(i).movimento(ma.get(i));
+		for (int i = 0; i < mot.size(); i ++) {
+			mot.get(i).movimento(mot.get(i));
 		}
 		
 		geraVeiculo();
 		detectaColisao();
-		refazMapa();
+		refazerMapa();
 		desenhaMundo();
 	}
 	
@@ -243,84 +243,84 @@ public class Mundo {
 	 */
 	public void detectaColisao() {
 		/// Colisão entre carros apenas
-		for (int i = 0; i < ca.size(); i ++) {
-			for (int j = 0; j < ca.size(); j++) {
-				if(ca.get(j).getX() == ca.get(i).getX() && ca.get(j).getY() == ca.get(i).getY()) {
+		for (int i = 0; i < car.size(); i ++) {
+			for (int j = 0; j < car.size(); j++) {
+				if(car.get(j).getX() == car.get(i).getX() && car.get(j).getY() == car.get(i).getY()) {
 					if (i == j) { /// Essa parte verifica se o veículo é ele mesmo, e não o remove
 						
 					}
 					else {
-						ca.remove(ca.get(i));
+						car.remove(car.get(i));
 					}	
 				}
 			}
 		}
 		
 		/// Colisão entre caminhões apenas
-		for (int i = 0; i < da.size(); i ++) {
-			for (int j = 0; j < da.size(); j++) {
-				if(da.get(j).getX() == da.get(i).getX() && da.get(j).getY() == da.get(i).getY()) {
+		for (int i = 0; i < cam.size(); i ++) {
+			for (int j = 0; j < cam.size(); j++) {
+				if(cam.get(j).getX() == cam.get(i).getX() && cam.get(j).getY() == cam.get(i).getY()) {
 					if(i == j) {
 						
 					}
 					else {
-						da.remove(da.get(i));
+						cam.remove(cam.get(i));
 					}	
 				}
 			}
 		}
 		
 		/// Colisão entre motos apenas
-		for (int i = 0; i < ma.size(); i ++) {
-			for (int j = 0; j < ma.size(); j++) {
-				if(ma.get(j).getX() == ma.get(i).getX() && ma.get(j).getY() == ma.get(i).getY()) {
+		for (int i = 0; i < mot.size(); i ++) {
+			for (int j = 0; j < mot.size(); j++) {
+				if(mot.get(j).getX() == mot.get(i).getX() && mot.get(j).getY() == mot.get(i).getY()) {
 					if(i == j) {
 						
 					}
 					else {
-						ma.remove(ma.get(i));
+						mot.remove(mot.get(i));
 					}	
 				}
 			}
 		}
 		
 		/// Colisão entre caminhão e carro
-		for(int i = 0; i < da.size(); i++) {
-			for (int j = 0; j < ca.size(); j++) {
-				if(ca.get(j).getX() == da.get(i).getX() && ca.get(j).getY() == da.get(i).getY()) {
-					if(da.size() > ca.size()) {
+		for(int i = 0; i < cam.size(); i++) {
+			for (int j = 0; j < car.size(); j++) {
+				if(car.get(j).getX() == cam.get(i).getX() && car.get(j).getY() == cam.get(i).getY()) {
+					if(cam.size() > car.size()) {
 						
 					}
 					else {
-						ca.remove(ca.get(j));
+						car.remove(car.get(j));
 					}
 				}
 			}
 		}
 		
 		/// Colisão entre caminhão e moto
-		for(int i = 0; i < da.size(); i++) {
-			for (int j = 0; j < ma.size(); j++) {
-				if(ma.get(j).getX() == da.get(i).getX() && ma.get(j).getY() == da.get(i).getY()) {
-					if(da.size() > ma.size()) {
+		for(int i = 0; i < cam.size(); i++) {
+			for (int j = 0; j < mot.size(); j++) {
+				if(mot.get(j).getX() == cam.get(i).getX() && mot.get(j).getY() == cam.get(i).getY()) {
+					if(cam.size() > mot.size()) {
 						
 					}
 					else {
-						ma.remove(ma.get(j));
+						mot.remove(mot.get(j));
 					}
 				}
 			}
 		}
 		
 		/// Colisão entre carro e moto
-		for(int i = 0; i < ca.size(); i++) {
-			for (int j = 0; j < ma.size(); j++) {
-				if(ma.get(j).getX() == ca.get(i).getX() && ma.get(j).getY() == ca.get(i).getY()) {
-					if(ca.size()>ma.size()) {
+		for(int i = 0; i < car.size(); i++) {
+			for (int j = 0; j < mot.size(); j++) {
+				if(mot.get(j).getX() == car.get(i).getX() && mot.get(j).getY() == car.get(i).getY()) {
+					if(car.size()>mot.size()) {
 						
 					}
 					else {
-						ma.remove(ma.get(i));
+						mot.remove(mot.get(i));
 					}
 					
 				}
@@ -332,30 +332,30 @@ public class Mundo {
 	 * Função que gera um veículo se passarem em fábricas
 	 */
 	public void geraVeiculo() {
-		for(int a = 0; a < ca.size(); a++) {
-			int x = ca.get(a).getX();
-			int y = ca.get(a).getY();
+		for(int a = 0; a < car.size(); a++) {
+			int x = car.get(a).getX();
+			int y = car.get(a).getY();
 				for(int i = 0; i < 30; i ++) {
 					for(int j = 0; j < 60; j++) {
 						if(i == x && j == y) {
-							if(mapa[i][j] == 2 && ca.get(a).getFabrica() == false) {
-								ca.get(a).setFabrica(true); /// Já que o veículo passou numa fábrica, ele fica impossibilitado de gerar novamente caso não saia da fabrica
-								ca.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
+							if(mapa[i][j] == 2 && car.get(a).getFabrica() == false) {
+								car.get(a).setFabrica(true); /// Já que o veículo passou numa fábrica, ele fica impossibilitado de gerar novamente caso não saia da fabrica
+								car.add(new Carro((v.setX()+1), (v.setY()+1), 2, "verde", false));
 								}
 							}
 						}
 					}
 				}
 		
-		for(int a = 0; a < da.size(); a++) {
-			int x = da.get(a).getX();
-			int y = da.get(a).getY();
+		for(int a = 0; a < cam.size(); a++) {
+			int x = cam.get(a).getX();
+			int y = cam.get(a).getY();
 				for(int i = 0; i < 30; i ++) {
 					for(int j = 0; j < 60; j++) {
 						if(i == x && j == y) {
-							if(mapa[i][j] == 2 && da.get(a).getFabrica() == false) {
-								da.get(a).setFabrica(true);
-								da.add(new Caminhao((v.setX()+1), (v.setY()+1), 1, "verde", false));
+							if(mapa[i][j] == 2 && cam.get(a).getFabrica() == false) {
+								cam.get(a).setFabrica(true);
+								cam.add(new Caminhao((v.setX()+1), (v.setY()+1), 1, "verde", false));
 								}
 							}
 						}
@@ -363,15 +363,15 @@ public class Mundo {
 				}
 		
 	
-		for(int a = 0; a < ma.size(); a++) {
-			int x = ma.get(a).getX();
-			int y = ma.get(a).getY();
+		for(int a = 0; a < mot.size(); a++) {
+			int x = mot.get(a).getX();
+			int y = mot.get(a).getY();
 				for(int i = 0; i < 30; i ++) {
 					for(int j = 0; j < 60; j++) {
 						if(i == x && j == y) {
-							if(mapa[i][j] == 2 && ma.get(a).getFabrica() == false) {
-								ma.get(a).setFabrica(true);
-								ma.add(new Moto((v.setX()+1), (v.setY()+1), 3, "vermelho", false));
+							if(mapa[i][j] == 2 && mot.get(a).getFabrica() == false) {
+								mot.get(a).setFabrica(true);
+								mot.add(new Moto((v.setX()+1), (v.setY()+1), 3, "vermelho", false));
 							}
 						}
 					}
